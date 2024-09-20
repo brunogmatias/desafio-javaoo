802c78dbb89bd1b29eecb00cf30e11959f028d3c
brunogmatias <bruno.gabriel0589@gmail.com>
2024-09-20 15:19:46 -0300

    Criação de parte do menu.

--- src/main/java/com/mycompany/javaoo/Javaoo.java
+++ src/main/java/com/mycompany/javaoo/Javaoo.java
@@ -5,13 +5,55 @@
 
 package com.mycompany.javaoo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author bruno
  */
+
+//classe principal
 public class Javaoo {
     
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //criado scanner entrada para leitura de dados.
+        Scanner entrada = new Scanner(System.in);
+        //criado o objeto gerenciador para usar os métodos da clase 'GerenciadorPessoas(Regra de Negócios)'.
+        GerenciadorPessoas gerenciador = new GerenciadorPessoas();
+        int op = 0;
+        
+        while(op != 5){           
+            switch(op){
+                 case 1:
+                     System.out.println("Pessoas cadastradas: ");
+                     gerenciador.listarPessoas();
+               break;
+               
+                 case 2:
+                    System.out.println("Digite o nome da pessoa: ");
+                    String nome = entrada.nextLine();
+                    System.out.println("Digite a função da pessoa: ");
+                    String funcao = entrada.nextLine();
+                    System.out.println("Digite o código da pessoa: ");
+                    int codigo = entrada.nextInt();
+                    
+                    gerenciador.cadastrarPessoas(nome, funcao, codigo);
+                break;
+                
+                 case 3:
+                     System.out.println("Digite o código da pessoa que você quer encontrar: ");
+                     int codE = entrada.nextInt();
+                     
+                     gerenciador.localizarPessoaCodigo(codE);
+                break;
+                
+                 case 4:
+                     System.out.println("Digite o código da pessoa que você quer remover: ");
+                     int codR = entrada.nextInt();
+                     
+                     
+                 break;
+            }      
+        }
     }
 }
