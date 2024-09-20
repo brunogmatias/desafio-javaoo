00e37fb4354dfa4b3ea7c80f192d42917a8b7a1b
brunogmatias <bruno.gabriel0589@gmail.com>
2024-09-20 20:45:11 -0300

    Organizei o código da classe principal para melhor leitura.

--- src/main/java/com/mycompany/javaoo/Javaoo.java
+++ src/main/java/com/mycompany/javaoo/Javaoo.java
@@ -22,62 +22,56 @@ public class Javaoo {
         GerenciadorPessoas gerenciador = new GerenciadorPessoas();
         int op = 0;
         
-        while(op != 5){
+        while(op != 5){          
+            System.out.println("\n--Controle de Funcionários--");
+            System.out.println("Digite a opção que deseja acesssar: \n");
             
-            System.out.println("--Controle de Funcionários--");
-            System.out.println("Digite a opção que deseja acesssar: ");
-            
-            System.out.println("1: Listar funcionários cadastrados.");
-            
-            System.out.println("2: Cadastrar novo funcionário.");
-            
-            System.out.println("3: Localizar funcionário por código.");
-            
-            System.out.println("4: Remover funcionário.");
-            
+            System.out.println("1: Listar funcionários cadastrados.");            
+            System.out.println("2: Cadastrar novo funcionário.");            
+            System.out.println("3: Localizar funcionário por código.");           
+            System.out.println("4: Remover funcionário.");           
             System.out.println("5: fechar programa.");
             op = entrada.nextInt();
             entrada.nextLine();
             
             switch(op){
-                 case 1:
-                     System.out.println("Pessoas cadastradas: ");
-                     gerenciador.listarPessoas();
-               break;
+                    case 1:
+                        System.out.println("Pessoas cadastradas: ");
+                        gerenciador.listarPessoas();
+                    break;
                
-                 case 2:
-                    System.out.println("Digite o nome da pessoa: ");
-                    String nome = entrada.nextLine();
-                   
-                    
-                    System.out.println("Digite a função da pessoa: ");
-                    String funcao = entrada.nextLine();
-                    
-                    System.out.println("Digite o código da pessoa: ");
-                    int codigo = entrada.nextInt();
-                    entrada.nextLine();
-                    
-                    gerenciador.cadastrarPessoas(nome, funcao, codigo);
-                break;
+                    case 2:
+                        System.out.println("Digite o nome da pessoa: ");
+                        String nome = entrada.nextLine();
+
+                        System.out.println("Digite a função da pessoa: ");
+                        String funcao = entrada.nextLine();
+
+                        System.out.println("Digite o código da pessoa: ");
+                        int codigo = entrada.nextInt();
+                        entrada.nextLine();
+
+                        gerenciador.cadastrarPessoas(nome, funcao, codigo);
+                    break;
                 
-                 case 3:
-                     System.out.println("Digite o código da pessoa que você quer encontrar: ");
-                     int codE = entrada.nextInt();
+                    case 3:
+                        System.out.println("Digite o código da pessoa que você quer encontrar: ");
+                        int codE = entrada.nextInt();
                      
-                     gerenciador.localizarPessoaCodigo(codE);
-                break;
+                        gerenciador.localizarPessoaCodigo(codE);
+                    break;
                 
-                 case 4:
-                     System.out.println("Digite o código da pessoa que você quer remover: ");
-                     int codR = entrada.nextInt();
+                    case 4:
+                        System.out.println("Digite o código da pessoa que você quer remover: ");
+                        int codR = entrada.nextInt();
                      
-                     gerenciador.removerPessoaCodigo(codR);
-                 break;
+                        gerenciador.removerPessoaCodigo(codR);
+                    break;
                  
-                 default:
-                  if(op != 5){
+                    default:
+                        if(op != 5){
                         System.out.println("Opção inválida.");
-                          } 
+                  } 
             }      
         }
         System.out.println("Programa finalizado. Até logo!");
